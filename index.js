23d67261ddac3d929b26747e0cf4ada37ab7bd97
will14lee <will14lee@gmail.com>
2021-06-18 11:46:33 -0400

    commit tree d169799988f191cc6485dc22edf8cbdb293d7d6d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const changeText= document.querySelector(`p`)
+    console.log("The DOM has loaded");
+    changeText.textContent= "This is really cool!"
+})
+console.log("This console.log() fires when index.js loades - before DOMContentLoaded is triggered")
+// const clickMe= document.querySelector(`button`)
+// document.addEventListener("click", function(){
+//     const changeText= document.querySelector(`p`)
+//     console.log("The DOM has loaded");
+//     changeText.textContent= "You clicked on me! Congrats!"
+// })
+// document.addEventListener('DOMContentLoaded', () => {
+//     const changeText= document.querySelector(`p`)
+//     console.log("The DOM has loaded");
+//     changeText.textContent= "This is really cool!"
+// });
